9a0b6706c3c408c5e93be03d8bd21e929179328b
Jang Wook Pyun <pyun0825@naver.com>
2018-09-13 22:54:53 +0900

    added phrase

--- src/vue/index.vue
+++ src/vue/index.vue
@@ -1,19 +1,20 @@
 var msg_list = [
-  'Hello, world!',
-  'KOrea univ. Software MaestrOS',
+  "Hello, world!",
+  "KOrea univ. Software MaestrOS",
+  "Eat, Code, Sleep"
   /* please fill in messages */
-]
+];
 
-var body = new Vue ({
-  el: '#body',
+var body = new Vue({
+  el: "#body",
   data: {
     shorten: false,
-    msg: ''
+    msg: ""
   },
   methods: {
     triggerMessage: function() {
       setInterval(() => {
-        this.msg = msg_list[Math.floor((Math.random() * msg_list.length))];
+        this.msg = msg_list[Math.floor(Math.random() * msg_list.length)];
       }, 3000);
     },
     triggerShorten: function() {
