e37f29e37105956a03dea54de9e43753b18a125b
서강욱 <tjrkddnr@korea.ac.kr>
2018-10-04 16:38:14 +0900

    initial settings

--- js/Game.js
+++ js/Game.js
@@ -1,6 +1,6 @@
 var camera, scene, renderer;
 var mesh;
-var frustrumSize = 1000;
+var frustumSize = 1000;
 
 if (WEBGL.isWebGLAvailable()) {
     init();
@@ -13,14 +13,53 @@ if (WEBGL.isWebGLAvailable()) {
 function init() {
 
     // Setup
-    renderer = new THREE.WebGLRenderer({ canvas: document.querySelector("#canvas") });
+    renderer = new THREE.WebGLRenderer({ canvas: document.getElementById('canvas'), antialis: true });
+    renderer.setClearColor(0x000000);
 
     // Make and setup a camera.
     var aspect = window.innerWidth / window.innerHeight;
-    camera = new THREE.OrthographicCamera(frustrumSize * aspect / -2, frustrumSize * aspect / 2, frustrumSize * aspect / 2, frustrumSize * aspect / -2, 0, 1000);
-    camera.position.z = 1;
+    camera = new THREE.OrthographicCamera(frustumSize * aspect / -2, frustumSize * aspect / 2, frustumSize * aspect / 2, frustumSize * aspect / -2, 0, 1000);
+    camera.position.z = 100;
 
     // Make a scene
     scene = new THREE.Scene();
 
+     //Make light
+     var Ambientlight = new THREE.AmbientLight(0xffffff, 0.5);
+     Ambientlight.position.z = 100;
+     scene.add(Ambientlight);
+ 
+     var PointLight = new THREE.PointLight(0xffffff, 0.5);
+     PointLight.position.z = 100;
+     scene.add(PointLight);
+
+    var geometry = new THREE.BoxGeometry(200,200,200);
+    var material = new THREE.MeshLambertMaterial({color: 0xff0000});
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.z = -100;
+    scene.add(mesh);
+    
+    // Make a Barrier
+    
+   
+}
+
+function resize() {
+    var width = renderer.domElement.clientWidth;
+	var height = renderer.domElement.clientHeight;
+	if (renderer.domElement.width !== width || renderer.domElement.height !== height) {
+		renderer.setSize(width, height, false);
+		camera.aspect = width / height;
+		camera.updateProjectionMatrix();
+	}
+}
+
+function animate() {
+    resize();
+
+    mesh.rotation.x += 0.05;
+    mesh.rotation.y += 0.05;
+
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate);
 }
