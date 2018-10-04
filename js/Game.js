308512e02892520edaa323bf3f1e845d7a0a17ad
kanguk Seo <tjrkddnr@korea.ac.kr>
2018-10-05 02:50:46 +0900

    draw grid

--- js/Game.js
+++ js/Game.js
@@ -17,30 +17,33 @@ function init() {
 
     // Make and setup a camera.
     var aspect = window.innerWidth / window.innerHeight;
-    camera = new THREE.OrthographicCamera(frustumSize * aspect / -2, frustumSize * aspect / 2, frustumSize * aspect / 2, frustumSize * aspect / -2, 0, 1000);
-    camera.position.z = 100;
-
+    camera = new THREE.PerspectiveCamera(120, aspect, 1, 1000);
+    camera.position.y = 500;
     // Make a scene
     scene = new THREE.Scene();
 
-     //Make light
-     var Ambientlight = new THREE.AmbientLight(0xffffff, 0.5);
-     Ambientlight.position.z = 100;
-     scene.add(Ambientlight);
+    //Make light
+    var Ambientlight = new THREE.AmbientLight(0xffffff, 0.5);
+    Ambientlight.position.set(0, 0, 0);
+    scene.add(Ambientlight);
  
-     var PointLight = new THREE.PointLight(0xffffff, 0.5);
-     PointLight.position.z = 100;
-     scene.add(PointLight);
+    var PointLight = new THREE.PointLight(0xffffff, 0.5);
+    PointLight.position.set(0, 0, 0);
+    scene.add(PointLight);
 
-    var geometry = new THREE.BoxGeometry(100,100,100);
-    var material = new THREE.MeshLambertMaterial({color: 0xff0000});
+    var geometry = new THREE.PlaneGeometry( 500, 500 );
+    var material = new THREE.MeshBasicMaterial( { color: 0x1f1f1f , side: THREE.DoubleSide } );
     mesh = new THREE.Mesh(geometry, material);
-    mesh.position.z = -100;
-    scene.add(mesh);
-    
-    // Make a Barrier
-    
+    mesh.position.set(0, 500, -300);
+    scene.add(mesh);    
+
+    var girdTable = new THREE.GridHelper(500, 10, 0xffffff );
+    girdTable.rotation.x = -Math.PI / 2;
+    girdTable.position.set(0, 500, -300);
+    scene.add( girdTable );
    
+    var gridFloor = new THREE.GridHelper(2000, 40, 0xffffff);
+    scene.add( gridFloor );
 }
 
 function resize() {
@@ -56,8 +59,10 @@ function resize() {
 function animate() {
     resize();
 
-    mesh.rotation.x += 0.05;
-    mesh.rotation.y += 0.05;
+    //mesh.rotation.x += 0.01;
+    //mesh.rotation.y += 0.01;
+    //mesh.rotation.z += 0.01;
+    //gridHelper.rotation.z += 0.01;
 
     renderer.render(scene, camera);
     requestAnimationFrame(animate);
